22592324e33cf4a4499747c57017feef0de0dcd2
djouher liza <kechemirdjouher@gmail.com>
2025-09-25 17:30:32 +0200

    djouher

--- boissons/high_level/tests.py
+++ boissons/high_level/tests.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
 
 from .models import Machine
+from .models import Local
+from .models import Localisation
 
 
 class MachineModelTests(TestCase):
     def test_machine_creation(self):
         self.assertEqual(Machine.objects.count(), 0)
+        lolo = Localisation.objects.create(nom="Lyon", taxes=20, prix_m2=6000)
+        toto = Local.objects.create(nom="Local303", localisation=lolo)
         Machine.objects.create(
             nom="Mélangeur",
             prix_achat=28_000,
@@ -15,5 +19,6 @@ class MachineModelTests(TestCase):
             surface=500,
             debit_energie=1200,
             taux_utilisation=24,
+            local=toto,
         )
         self.assertEqual(Machine.objects.count(), 1)
