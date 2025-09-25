fcaa23776de05fb17a5243d451dc06cd6c98166f
djouher liza <kechemirdjouher@gmail.com>
2025-09-25 16:34:41 +0200

    mod

--- boissons/high_level/tests.py
+++ boissons/high_level/tests.py
@@ -1,1 +1,10 @@
-# Create your tests here.
+from django.test import TestCase
+
+from .models import Machine
+
+
+class MachineModelTests(TestCase):
+    def test_machine_creation(self):
+        self.assertEqual(Machine.objects.count(), 0)
+        Machine.objects.create(nom="Mélangeur", prix=28_000)
+        self.assertEqual(Machine.objects.count(), 1)
