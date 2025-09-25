8190f5c446cbc78017e48595040c953dd0ad12a4
djouher liza <kechemirdjouher@gmail.com>
2025-09-25 17:11:01 +0200

    djouher

--- boissons/high_level/tests.py
+++ boissons/high_level/tests.py
@@ -6,5 +6,14 @@ from .models import Machine
 class MachineModelTests(TestCase):
     def test_machine_creation(self):
         self.assertEqual(Machine.objects.count(), 0)
-        Machine.objects.create(nom="Mélangeur", prix_achat=28_000)
+        Machine.objects.create(
+            nom="Mélangeur",
+            prix_achat=28_000,
+            cout_maintenance=3000,
+            operateur="op1",
+            debit=1200,
+            surface=500,
+            debit_energie=1200,
+            taux_utilisation=24,
+        )
         self.assertEqual(Machine.objects.count(), 1)
