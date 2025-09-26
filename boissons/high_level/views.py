13d2db8a62523ca39c8cb9a08f66ac0592e62ef1
djouher liza <kechemirdjouher@gmail.com>
2025-09-26 11:55:52 +0200

    mess

--- boissons/high_level/views.py
+++ boissons/high_level/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.views.generic import DetailView, ListView
+from .models import Boissons
+
+
+class BoissonsListView(ListView):
+    model = Boissons
+
+
+class BoissonsDetailView(DetailView):
+    model = Boissons
