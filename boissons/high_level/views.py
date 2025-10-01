e207c2744cdbb22f32dba0bf030a87035bfcd2ab
djouher liza <kechemirdjouher@gmail.com>
2025-10-01 09:18:09 +0200

    message

--- boissons/high_level/views.py
+++ boissons/high_level/views.py
@@ -1,10 +1,80 @@
-from django.views.generic import DetailView, ListView
-from .models import Boissons
+from django.views.generic import DetailView
+from django.http import Jsonresponse
+from . import models
 
 
-class BoissonsListView(ListView):
-    model = Boissons
+class LocalisationDetailView(DetailView):
+    model = models.Localisation
+
+    def render_to_response(self):
+        return Jsonresponse(self.object.json)
 
 
-class BoissonsDetailView(DetailView):
-    model = Boissons
+class LocalDetailView(DetailView):
+    model = models.Local
+
+    def render_to_response(self):
+        return Jsonresponse(self.object.json)
+
+
+class EnergieDetailView(DetailView):
+    model = models.Energie
+
+    def render_to_response(self):
+        return Jsonresponse(self.object.json)
+
+
+class DebitEnergieDetailView(DetailView):
+    model = models.DebitEnergie
+
+    def render_to_response(self):
+        return Jsonresponse(self.object.json)
+
+
+class ProduitDetailView(DetailView):
+    model = models.Produit
+
+    def render_to_response(self):
+        return Jsonresponse(self.object.json)
+
+
+class MetierDetailView(DetailView):
+    model = models.Metier
+
+    def render_to_response(self):
+        return Jsonresponse(self.object.json)
+
+
+class ApprovisionnementMatierePremiereDetailView(DetailView):
+    model = models.ApprovisionnementMatierePremiere
+
+    def render_to_response(self):
+        return Jsonresponse(self.object.json)
+
+
+class RessourceHumaineDetailView(DetailView):
+    model = models.RessourceHumaine
+
+    def render_to_response(self):
+        return Jsonresponse(self.object.json)
+
+
+class FabricationDetailView(DetailView):
+    model = models.Fabrication
+
+    def render_to_response(self):
+        return Jsonresponse(self.object.json)
+
+
+class MachineDetailView(DetailView):
+    model = models.Machine
+
+    def render_to_response(self):
+        return Jsonresponse(self.object.json)
+
+
+class MatierePremiereDetailView(DetailView):
+    model = models.MatierePremiere
+
+    def render_to_response(self):
+        return Jsonresponse(self.object.json)
